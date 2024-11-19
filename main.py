1e1afd5fb214f832752b6fc34efcd06a91492b3d
KingKwenaite <165156543+KingKwenaite@users.noreply.github.com>
2024-11-19 08:27:48 +0200

    First code with matplotlib

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1,3,9,5,11])
+ypoints = np.array([2,6,4,8,12])
+#plt.plot(xpoints,ypoints)
+#plt.plot(xpoints,ypoints,'0')
+plt.plot(xpoints,'o')
+plt.show()
